e2ad9965c09a10bfb2e9fc55ab8aa9e5d924ee69
Sergio Moreno <moresergioval@gmail.com>
2024-01-21 12:28:50 +0100

    ComponenteServicios

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,11 +2,13 @@ import { ModuleWithProviders } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { EspecificacionesComponent } from "./components/especificaciones/especificaciones.component";
 import { HomeComponent } from "./components/home/home.component";
+import {ServiciosComponent} from "./components/servicios/servicios.component"
 
 
 const appRoutes: Routes = [
   { path: "", component: HomeComponent },
   { path: "espe", component: EspecificacionesComponent },
+  { path: "servicios", component: ServiciosComponent },
   
 
 ]
